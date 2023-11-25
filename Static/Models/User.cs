8e7f15c91c2e0bb8a150bfd010a2075e227b4391
Nihadname <nihadcoding@gmail.com>
2023-11-25 15:13:43 +0400

    this is finished

--- Static/Models/User.cs
+++ Static/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Static.InterFaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Static.Models
 {
-    public class User
+    public class User: IAccount
     {
         public static int IdCounter;
         public int Id;
